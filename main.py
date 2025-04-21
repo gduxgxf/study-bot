5741a0f281cdff4d607efd138efa6c1353ce237a
gduxgxf <ynhnjmjj@gmail.com>
2025-04-21 20:23:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import requests
 import datetime
+
+# توكن البوت
+BOT_TOKEN = '8054601472:AAHSX4eJYUDomEv1__SQlZGr-kb4yN0HsRA'
+# معرف المستخدم (User ID)
+USER_ID = '1951801385'
 
 def send_reminder():
     now = datetime.datetime.now()
-    print(f"تذكير بالمذاكرة! الوقت الحالي: {now.strftime('%Y-%m-%d %H:%M:%S')}")
+    message = f"تذكير بالمذاكرة! الوقت الحالي: {now.strftime('%Y-%m-%d %H:%M:%S')}"
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {
+        "chat_id": USER_ID,
+        "text": message
+    }
+    requests.post(url, data=data)
 
 send_reminder()
